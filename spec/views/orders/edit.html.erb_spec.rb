3c4a124883875c7ddf8a2e7eb33beaa3bcd31041
Allen Zhong <zhong.allen@gmail.com>
2016-11-08 20:54:37 +1300

    add view spec

--- spec/views/orders/edit.html.erb_spec.rb
+++ spec/views/orders/edit.html.erb_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe "orders/edit", type: :view do
     render
 
     assert_select "form[action=?][method=?]", order_path(@order), "post" do
+      assert_select "select#order_status[name=?]", "order[status]"
+      assert_select "div#order_items", :count => 1
     end
   end
 end
--- spec/views/orders/new.html.erb_spec.rb
+++ spec/views/orders/new.html.erb_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "orders/new", type: :view do
   before(:each) do
-    assign(:order, FactoryGirl.create(:order))
+    assign(:order, FactoryGirl.build(:order))
   end
 
   it "renders new order form" do
     render
-    assert_select "form[action=?][method=?]", orders_path, "post" do
+    assert_select "form#new_order[action=?][method=?]", "/orders", "post" do
+      assert_select "select#order_status[name=?]", "order[status]"
+      assert_select "div#order_items", :count => 1
     end
   end
 end
