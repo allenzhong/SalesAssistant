3961126b7fe010966cf8d1bb1d548b6147aef069
Allen Zhong <zhong.allen@gmail.com>
2016-12-17 22:57:17 +1300

    change test

--- spec/views/orders/edit.html.erb_spec.rb
+++ spec/views/orders/edit.html.erb_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "orders/edit", type: :view do
   before(:each) do
-    @order = assign(:order, FactoryGirl.create(:order))
+    @order = assign(:order, FactoryGirl.create(:order_with_items))
   end
 
   it "renders the edit order form" do
@@ -10,7 +10,8 @@ RSpec.describe "orders/edit", type: :view do
 
     assert_select "form[action=?][method=?]", order_path(@order), "post" do
       assert_select "select#order_status[name=?]", "order[status]"
-      assert_select "div#order_items", :count => 1
+      assert_select "div#order-items", :count => 1
+      assert_select "div.nested-fields", :count => 5
     end
   end
 end
