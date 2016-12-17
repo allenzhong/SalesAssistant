c6735df108e52698bb66364358da8cf9881023c0
Allen Zhong <zhong.allen@gmail.com>
2016-12-17 22:57:17 +1300

    change test

--- spec/views/orders/new.html.erb_spec.rb
+++ spec/views/orders/new.html.erb_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "orders/new", type: :view do
   before(:each) do
-    assign(:order, FactoryGirl.build(:order))
+    assign(:order, FactoryGirl.build(:order_with_items))
   end
 
   it "renders new order form" do
     render
     assert_select "form#new_order[action=?][method=?]", "/orders", "post" do
       assert_select "select#order_status[name=?]", "order[status]"
-      assert_select "div#order_items", :count => 1
+      assert_select "div#order-items", :count => 1
     end
   end
 end
